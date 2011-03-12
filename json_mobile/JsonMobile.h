491a6be547cf6539ce62dd5ac7e4e0d8c14c241c
João Magalhães <joamag@hive.pt>
2011-03-12 13:38:50 +0000

    new files added

---
+++ json_mobile/JsonMobile.h
@@ -0,0 +1,16 @@
+//
+//  JsonMobile.h
+//  json_mobile
+//
+//  Created by João Magalhães on 3/11/11.
+//  Copyright 2011 Hive Solutions Lda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface JsonMobile : NSObject {
+    
+}
+
+@end
---
+++ json_mobile/JsonMobile.m
@@ -0,0 +1,14 @@
+//
+//  JsonMobile.m
+//  json_mobile
+//
+//  Created by João Magalhães on 3/11/11.
+//  Copyright 2011 Hive Solutions Lda. All rights reserved.
+//
+
+#import "JsonMobile.h"
+
+
+@implementation JsonMobile
+
+@end
